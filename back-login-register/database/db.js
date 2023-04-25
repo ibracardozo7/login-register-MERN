5f9126d43f24aae7321d89761a2dc1b8f957f296
Ibrahim Cardozo <ibracardozo15@gmail.com>
2023-04-25 17:56:22 -0300

    modification

--- back-login-register/database/db.js
+++ back-login-register/database/db.js
@@ -1,17 +1,31 @@
 const mongoose = require('mongoose')
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 // const MONGO_URL = "mongodb://localhost/autenticacionLocal"
 const url = process.env.MONGODB_URL
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 const db = async () => {
-   await mongoose.connect(url, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-   })
-   .then(() => console.log("DB FUNCIONANDO"))
-   .catch((error) => console.error(error))
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
 
 module.exports = db
