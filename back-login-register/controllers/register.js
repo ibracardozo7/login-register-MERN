8c48daf6fd3c7fc22cb3d8d15b7b1d0d717630c2
Ibrahim Cardozo <ibracardozo15@gmail.com>
2023-04-25 19:04:15 -0300

    modification

--- back-login-register/controllers/register.js
+++ back-login-register/controllers/register.js
@@ -2,14 +2,14 @@ const bcrypt = require("bcrypt");
 const Usuario = require("../model/usuario");
 
 const register = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Max-Age", "1800");
+  // res.setHeader("Access-Control-Allow-Headers", "content-type");
+  // res.setHeader(
+  //   "Access-Control-Allow-Methods",
+  //   "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  // );
   const { nombre, correo, contraseña } = req.body;
 
   Usuario.findOne({ correo }).then((usuario) => {
